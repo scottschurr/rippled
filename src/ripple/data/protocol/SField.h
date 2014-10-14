c92f8d0d97e05779ce742f11dd3ca5329972f20e
Scott Schurr <scott@ripple.com>
2014-10-14 11:01:07 -0700

    Functional multi-signature validation [RIPD-480]
    This implementation is proof of concept only.  The validation
    is in the wrong place.  Future commits will move the signature
    validation into the transaction engine.

--- src/ripple/data/protocol/SField.h
+++ src/ripple/data/protocol/SField.h
@@ -110,13 +110,20 @@ public:
         sMD_Default        = sMD_ChangeOrig | sMD_ChangeNew | sMD_DeleteFinal | sMD_Create
     };
 
-    const int               fieldCode;      // (type<<16)|index
-    const SerializedTypeID  fieldType;      // STI_*
-    const int               fieldValue;     // Code number for protocol
+    enum class IsSigning : unsigned char
+    {
+        no,
+        yes
+    };
+    static IsSigning const notSigning = IsSigning::no;
+
+    int const               fieldCode;      // (type<<16)|index
+    SerializedTypeID const  fieldType;      // STI_*
+    int const               fieldValue;     // Code number for protocol
     std::string             fieldName;
     int                     fieldMeta;
     int                     fieldNum;
-    bool                    signingField;
+    IsSigning               signingField;
     std::string             rawJsonName;
     Json::StaticString      jsonName;
 
@@ -141,7 +148,7 @@ public:
 private:
     // These constructors can only be called from FieldNames.cpp
     SField (SerializedTypeID tid, int fv, const char* fn,
-            int meta = sMD_Default, bool signing = true);
+            int meta = sMD_Default, IsSigning signing = IsSigning::yes);
     explicit SField (int fc);
     SField (SerializedTypeID id, int val);
 
@@ -214,11 +221,11 @@ public:
 
     bool isSigningField () const
     {
-        return signingField;
+        return signingField == IsSigning::yes;
     }
     void notSigningField ()
     {
-        signingField = false;
+        signingField = IsSigning::no;
     }
     bool shouldMeta (int c) const
     {
@@ -231,7 +238,8 @@ public:
 
     bool shouldInclude (bool withSigningField) const
     {
-        return (fieldValue < 256) && (withSigningField || signingField);
+        return (fieldValue < 256) &&
+            (withSigningField || (signingField == IsSigning::yes));
     }
 
     bool operator== (const SField& f) const
