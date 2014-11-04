dacb90d449f02b07ae0e266071060b6513c2ddae
Scott Schurr <scott@ripple.com>
2014-11-04 15:51:53 -0800

    Successful multi-signature transaction validation (RIPD-480]
    I've seen one multi-signed transaction succeed with this code.
    And I've seen no failures (yet).  However there is stuff further
    down stream from the signature check that causes the transaction
    to fail.  So we're not out of the woods yet.
    
    Submitting to github so reviewers can tell me if the code is in
    the right place and what they would like to have changed.

--- src/ripple/unity/app9.cpp
+++ src/ripple/unity/app9.cpp
@@ -41,3 +41,4 @@
 #include <ripple/app/transactors/CreateTicket.cpp>
 #include <ripple/app/transactors/CancelTicket.cpp>
 #include <ripple/app/transactors/SetSignerList.cpp>
+#include <ripple/app/transactors/Impl/SignerEntries.cpp>
