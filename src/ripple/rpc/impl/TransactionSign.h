9932d9d2df492683a646e9fa8281553ea0bd6207
Scott Schurr <scott@ripple.com>
2015-09-02 16:58:49 -0700

    Remove TxnSignApiFacade (RIPD-945):
    Replace TxnSignApiFacade with separate passed in arguments to
    the various sign/submit RPC commands.  Also increase unit test
    coverage of the submit_multisign RPC command.

--- src/ripple/rpc/impl/TransactionSign.h
+++ src/ripple/rpc/impl/TransactionSign.h
@@ -20,8 +20,9 @@
 #ifndef RIPPLE_RPC_TRANSACTIONSIGN_H_INCLUDED
 #define RIPPLE_RPC_TRANSACTIONSIGN_H_INCLUDED
 
+#include <ripple/app/ledger/LedgerMaster.h>
 #include <ripple/app/misc/NetworkOPs.h>
-#include <ripple/protocol/ErrorCodes.h>
+#include <ripple/app/main/Application.h>     // getApp()
 #include <ripple/server/Role.h>
 
 namespace ripple {
@@ -29,102 +30,38 @@ namespace RPC {
 
 namespace detail {
 
-// A class that allows these methods to be called with or without a
-// real NetworkOPs instance.  This allows for unit testing.
-class TxnSignApiFacade
-{
-private:
-    NetworkOPs* const netOPs_;
-    std::shared_ptr<ReadView const> ledger_;
-    AccountID accountID_;
-    std::shared_ptr<SLE const> sle_;
-
-public:
-    // Enum used to construct a Facade for unit tests.
-    enum NoNetworkOPs{
-        noNetOPs
-    };
-
-    TxnSignApiFacade () = delete;
-    TxnSignApiFacade (TxnSignApiFacade const&) = delete;
-    TxnSignApiFacade& operator= (TxnSignApiFacade const&) = delete;
-
-    // For use in non unit testing circumstances.
-    explicit TxnSignApiFacade (NetworkOPs& netOPs)
-    : netOPs_ (&netOPs)
-    { }
-
-    // For testTransactionRPC unit tests.
-    explicit TxnSignApiFacade (NoNetworkOPs noOPs)
-    : netOPs_ (nullptr) { }
-
-    // For testAutoFillFees unit tests.
-    TxnSignApiFacade (NoNetworkOPs noOPs, std::shared_ptr<ReadView const> ledger)
-    : netOPs_ (nullptr)
-    , ledger_ (ledger)
-    { }
-
-    void snapshotAccountState (AccountID const& accountID);
-
-    bool isValidAccount () const;
-
-    std::uint32_t getSeq () const;
-
-    bool findPathsForOneIssuer (
-        AccountID const& dstAccountID,
-        Issue const& srcIssue,
-        STAmount const& dstAmount,
-        int searchLevel,
-        unsigned int const maxPaths,
-        STPathSet& pathsOut,
-        STPath& fullLiquidityPath) const;
-
-    void processTransaction (
-        Transaction::pointer& transaction,
-        bool bAdmin,
-        bool bLocal,
-        NetworkOPs::FailHard failType);
-
-    std::uint64_t scaleFeeBase (std::uint64_t fee) const;
-
-    std::uint64_t scaleFeeLoad (std::uint64_t fee, bool bAdmin) const;
-
-    bool hasAccountRoot () const;
-
-    error_code_i singleAcctMatchesPubKey (
-        RippleAddress const& publicKey) const;
-
-    error_code_i multiAcctMatchesPubKey (
-        AccountID const& acctID,
-        RippleAddress const& publicKey) const;
-
-    int getValidatedLedgerAge () const;
-
-    bool isLoadedCluster () const;
-};
-
 // A function to auto-fill fees.
-enum class AutoFill : unsigned char
-{
-    dont,
-    might
-};
-
 Json::Value checkFee (
     Json::Value& request,
-    TxnSignApiFacade& apiFacade,
     Role const role,
-    AutoFill const doAutoFill);
+    bool doAutoFill,
+    LoadFeeTrack const& feeTrack,
+    std::shared_ptr<ReadView const>& ledger);
+
+// Return a std::function<> that calls NetworkOPs::processTransaction.
+using ProcessTransactionFn =
+    std::function<void (Transaction::pointer& transaction,
+        bool bAdmin, bool bLocal, NetworkOPs::FailHard failType)>;
+
+inline ProcessTransactionFn getProcessTxnFn (NetworkOPs& netOPs)
+{
+    return [&netOPs](Transaction::pointer& transaction,
+        bool bAdmin, bool bLocal, NetworkOPs::FailHard failType)
+    {
+        netOPs.processTransaction(transaction, bAdmin, bLocal, failType);
+    };
+}
 
 } // namespace detail
 
-
 /** Returns a Json::objectValue. */
 Json::Value transactionSign (
     Json::Value params,  // Passed by value so it can be modified locally.
     NetworkOPs::FailHard failType,
-    detail::TxnSignApiFacade& apiFacade,
-    Role role);
+    Role role,
+    int validatedLedgerAge,
+    LoadFeeTrack const& feeTrack,
+    std::shared_ptr<ReadView const> ledger);
 
 /** Returns a Json::objectValue. */
 inline
@@ -134,16 +71,24 @@ Json::Value transactionSign (
     NetworkOPs& netOPs,
     Role role)
 {
-    detail::TxnSignApiFacade apiFacade (netOPs);
-    return transactionSign (params, failType, apiFacade, role);
+    return transactionSign (
+        params,
+        failType,
+        role,
+        getApp().getLedgerMaster().getValidatedLedgerAge(),
+        getApp().getFeeTrack(),
+        getApp().getLedgerMaster().getCurrentLedger());
 }
 
 /** Returns a Json::objectValue. */
 Json::Value transactionSubmit (
     Json::Value params,  // Passed by value so it can be modified locally.
     NetworkOPs::FailHard failType,
-    detail::TxnSignApiFacade& apiFacade,
-    Role role);
+    Role role,
+    int validatedLedgerAge,
+    LoadFeeTrack const& feeTrack,
+    std::shared_ptr<ReadView const> ledger,
+    detail::ProcessTransactionFn const& processTransaction);
 
 /** Returns a Json::objectValue. */
 inline
@@ -153,16 +98,24 @@ Json::Value transactionSubmit (
     NetworkOPs& netOPs,
     Role role)
 {
-    detail::TxnSignApiFacade apiFacade (netOPs);
-    return transactionSubmit (params, failType, apiFacade, role);
+    return transactionSubmit (
+        params,
+        failType,
+        role,
+        getApp().getLedgerMaster().getValidatedLedgerAge(),
+        getApp().getFeeTrack(),
+        getApp().getLedgerMaster().getCurrentLedger(),
+        detail::getProcessTxnFn (netOPs));
 }
 
 /** Returns a Json::objectValue. */
 Json::Value transactionSignFor (
     Json::Value params,  // Passed by value so it can be modified locally.
     NetworkOPs::FailHard failType,
-    detail::TxnSignApiFacade& apiFacade,
-    Role role);
+    Role role,
+    int validatedLedgerAge,
+    LoadFeeTrack const& feeTrack,
+    std::shared_ptr<ReadView const> ledger);
 
 /** Returns a Json::objectValue. */
 inline
@@ -172,16 +125,24 @@ Json::Value transactionSignFor (
     NetworkOPs& netOPs,
     Role role)
 {
-    detail::TxnSignApiFacade apiFacade (netOPs);
-    return transactionSignFor (params, failType, apiFacade, role);
+    return transactionSignFor (
+        params,
+        failType,
+        role,
+        getApp().getLedgerMaster().getValidatedLedgerAge(),
+        getApp().getFeeTrack(),
+        getApp().getLedgerMaster().getCurrentLedger());
 }
 
 /** Returns a Json::objectValue. */
 Json::Value transactionSubmitMultiSigned (
     Json::Value params,  // Passed by value so it can be modified locally.
     NetworkOPs::FailHard failType,
-    detail::TxnSignApiFacade& apiFacade,
-    Role role);
+    Role role,
+    int validatedLedgerAge,
+    LoadFeeTrack const& feeTrack,
+    std::shared_ptr<ReadView const> ledger,
+    detail::ProcessTransactionFn const& processTransaction);
 
 /** Returns a Json::objectValue. */
 inline
@@ -191,8 +152,14 @@ Json::Value transactionSubmitMultiSigned (
     NetworkOPs& netOPs,
     Role role)
 {
-    detail::TxnSignApiFacade apiFacade (netOPs);
-    return transactionSubmitMultiSigned (params, failType, apiFacade, role);
+    return transactionSubmitMultiSigned (
+        params,
+        failType,
+        role,
+        getApp().getLedgerMaster().getValidatedLedgerAge(),
+        getApp().getFeeTrack(),
+        getApp().getLedgerMaster().getCurrentLedger(),
+        detail::getProcessTxnFn (netOPs));
 }
 
 } // RPC
